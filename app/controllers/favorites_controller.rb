e3bbb8deb204452c7d097518d217a13c93cb0274
daisukeyamaguchi329 <gaman55894@gmail.com>
2021-10-23 03:43:02 +0000

    Rayout Commit3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,12 @@
 class FavoritesController < ApplicationController
   before_action :set_post
   before_action :authenticate_user!
-  
+
   def index
+    @post = current_user.favorite.includes(:user).order(created_at: :desc)
   end
-  
-  
+
+
   def create
       @post = Post.find(params[:post_id])
     if @post.user_id != current_user.id   # 投稿者本人以外に限定
@@ -18,12 +19,12 @@ class FavoritesController < ApplicationController
     @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
     @favorite.destroy
   end
-  
+
   private
   def set_post
     @post = Post.find(params[:post_id])
   end
-  
+
   def favorite_params
     params.require(:favorite).permit(:user_id, :post_id)
   end
