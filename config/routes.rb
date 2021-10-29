0bdcc76a3cc29760053c002c8d110588fc42ae00
daisukeyamaguchi329 <gaman55894@gmail.com>
2021-10-29 15:09:59 +0000

    Layout Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,13 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show edit update]
 
-  get 'users/quit' => 'users#quit'
-  get 'users/out' => 'users#out'
   resources :posts, only: %i[index show new create edit update destroy] do
     resources :reviews, only: %i[index new create destroy]
     resources :favorites, only: %i[create destroy]
   end
-  
+
   get 'favorites', to: 'favorites#index'
- 
+
   resources :contacts, only: %i[new create]
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
