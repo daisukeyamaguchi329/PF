052dcdb7dd476a481fb5657b0184bae277213dce
daisukeyamaguchi329 <gaman55894@gmail.com>
2021-10-29 17:06:50 +0000

    Back Commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -36,6 +36,6 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :email, :subject, :message)
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
